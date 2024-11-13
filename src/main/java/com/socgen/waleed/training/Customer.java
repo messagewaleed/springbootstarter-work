1a96063c0eaa3d79f580f51502c2abee28f4ef85
waq <messagewaleed@gmail.com>
2024-11-13 17:38:37 +0530

    Added the code for Read, Add Customers - A Rest API with Spring boot

---
+++ src/main/java/com/socgen/waleed/training/Customer.java
@@ -0,0 +1,5 @@
+package com.socgen.waleed.training;
+
+public record Customer(Integer accountNumber, String name, Long balance) {
+
+}
--- src/main/java/com/socgen/waleed/training/SpringbootstarterWorkApplication.java
+++ src/main/java/com/socgen/waleed/training/SpringbootstarterWorkApplication.java
@@ -2,12 +2,50 @@ package com.socgen.waleed.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
 public class SpringbootstarterWorkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootstarterWorkApplication.class, args);
 	}
+	
+//	@RequestMapping("/message")
+//	String getMessage() {
+//		return "hey there, howdy?";
+//	}
+//	
+//	@RequestMapping("/greeting")
+//	String getGreeting() {
+//		return "Good evening";
+//	}
+	
+	
+//Customers's Rest API	
+//- /customers - All the customers - get
+//- /customers/id - Get a customer by id - get
+//- /customers - Add a new customer - post
+//- /customers/id - Update a customer - put
+//- /customers/id - Delete a customer - delete
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
